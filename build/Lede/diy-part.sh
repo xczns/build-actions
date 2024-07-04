390dbbf7c4f6269c9144f2e398e3d045eaa62895
xcz <zyb183328@email.swu.edu.cn>
2024-07-04 15:06:31 +0800

    更新 diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -100,7 +100,6 @@ openwrt-x86-64-generic-kernel.bin
 openwrt-x86-64-generic.manifest
 openwrt-x86-64-generic-squashfs-rootfs.img.gz	
 openwrt-rockchip-armv8-friendlyarm_nanopi-r2s.manifest
-ipk.tar.gz
 EOF
 
 # 在线更新时，删除不想保留固件的某个文件，在EOF跟EOF之间加入删除代码，记住这里对应的是固件的文件路径，比如： rm -rf /etc/config/luci
